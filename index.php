670a43d5c152d088e5d76a7b6e71bd0287b72318
niccov <niccoventuri27@gmail.com>
2023-04-19 19:01:07 +0200

    password generated

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
 
 
-$permitted_chars  =  '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ£!&%?#' ;
 
-function generatePassword($_GET['passwordLength']) {
+
+function generatePassword($passwordLength) {
+
+    $permitted_chars  =  '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ£!&%?#' ;
 
     $input_length = strlen($permitted_chars);
     $random_string = '';
-    for($i = 0; $i < $_GET['passwordLength']; $i++) {
+    for($i = 0; $i < $passwordLength; $i++) {
         $random_character = $permitted_chars[mt_rand(0, $input_length - 1)];
         $random_string .= $random_character;
     }
@@ -26,10 +28,13 @@ function generatePassword($_GET['passwordLength']) {
 </head>
 <body>
     <div>
+       
         <form action="index.php" method="GET">
             <input type="number"  min="0" max="10" name="passwordLength">
-            <input type="submit" <?php echo generatePassword() ?>>
+            <input type="submit" >
         </form>
+
+        <?php echo generatePassword($_GET['passwordLength']) ?>
     </div>
 </body>
 </html>
